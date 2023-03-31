f389e66750249f35203ae288887e38b559d75f1c
tsumuchan <tsumuchan.app@gmail.com>
2023-04-01 08:51:38 +0900

    create post2slack base code

---
+++ post2slack.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title post2slack
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+
+# Documentation:
+# @raycast.author tsumuchan
+
+puts "Hello World! Argument1 value: " + ARGV[0]
+
